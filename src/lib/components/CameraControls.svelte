078f619b914aa73f04eb1bfa7b11a30b7088ef51
Jia En <tujiaen@gmail.com>
2025-03-20 16:44:37 +0800

    Improve slider styling

--- src/lib/components/CameraControls.svelte
+++ src/lib/components/CameraControls.svelte
@@ -65,22 +65,46 @@
   >
 </div>
 
-<div class="fixed top-0 right-0 h-full flex flex-col justify-center z-2 mr-4">
+<div
+  class="fixed top-0 right-0 h-full flex flex-col justify-center items-center gap-y-2 z-2 mr-4"
+>
   <input
     type="range"
     bind:this={sharedState.zoom.sliderElement}
     disabled={!sharedState.zoom.isSupported}
     class={!sharedState.zoom.isSupported ? "hidden" : ""}
   />
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    fill="#fff"
+    viewBox="0 0 256 256"
+    ><path
+      d="M152,112a8,8,0,0,1-8,8H120v24a8,8,0,0,1-16,0V120H80a8,8,0,0,1,0-16h24V80a8,8,0,0,1,16,0v24h24A8,8,0,0,1,152,112Zm77.66,117.66a8,8,0,0,1-11.32,0l-50.06-50.07a88.11,88.11,0,1,1,11.31-11.31l50.07,50.06A8,8,0,0,1,229.66,229.66ZM112,184a72,72,0,1,0-72-72A72.08,72.08,0,0,0,112,184Z"
+    ></path></svg
+  >
 </div>
 
-<div class="fixed top-0 left-0 h-full flex flex-col justify-center z-2 mr-4">
+<div
+  class="fixed top-0 left-0 h-full flex flex-col justify-center items-center gap-y-2 z-2 ml-4"
+>
   <input
     type="range"
     bind:this={sharedState.exposure.sliderElement}
     disabled={!sharedState.exposure.isSupported}
     class={!sharedState.exposure.isSupported ? "hidden" : ""}
   />
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    fill="#fff"
+    viewBox="0 0 256 256"
+    ><path
+      d="M205.66,61.66l-144,144a8,8,0,0,1-11.32-11.32l144-144a8,8,0,0,1,11.32,11.32ZM64,112a8,8,0,0,0,16,0V80h32a8,8,0,0,0,0-16H80V32a8,8,0,0,0-16,0V64H32a8,8,0,0,0,0,16H64Zm160,64H144a8,8,0,0,0,0,16h80a8,8,0,0,0,0-16Z"
+    ></path></svg
+  >
 </div>
 
 <style>
